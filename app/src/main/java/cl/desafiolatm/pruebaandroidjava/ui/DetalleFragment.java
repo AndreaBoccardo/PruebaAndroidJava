2ec8a8229b674d3f1f40aa959ec738fea2ec8cf0
Andrea Boccardo <andrea.boccardo@gmail.com>
2022-02-21 16:33:36 -0300

    Se realiza el viewBinding y dataBinding en los elementos correspondientes

--- app/src/main/java/cl/desafiolatm/pruebaandroidjava/ui/DetalleFragment.java
+++ app/src/main/java/cl/desafiolatm/pruebaandroidjava/ui/DetalleFragment.java
@@ -2,24 +2,38 @@ package cl.desafiolatm.pruebaandroidjava.ui;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
+
 import cl.desafiolatm.pruebaandroidjava.R;
 import cl.desafiolatm.pruebaandroidjava.databinding.FragmentDetalleBinding;
+import cl.desafiolatm.pruebaandroidjava.viewmodel.HeroeViewModel;
 
 public class DetalleFragment extends Fragment {
 
     private FragmentDetalleBinding binding;
+    private HeroeViewModel modelo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentDetalleBinding.inflate(inflater,container,false);
+        modelo = new ViewModelProvider(getActivity()).get(HeroeViewModel.class);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.titulo_fragmento_detalle);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        binding.setSuperHeroe(modelo.getSuperHeroe().getValue());
 
+        modelo.getSuperHeroe().observe(getViewLifecycleOwner(), item -> {
+            Picasso.get().load(String.valueOf(item.getImages())).into(binding.ivHeroeItem);
+        });
 
         return binding.getRoot();
     }
--- app/src/main/java/cl/desafiolatm/pruebaandroidjava/ui/ListaFragment.java
+++ app/src/main/java/cl/desafiolatm/pruebaandroidjava/ui/ListaFragment.java
@@ -2,24 +2,52 @@ package cl.desafiolatm.pruebaandroidjava.ui;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import cl.desafiolatm.pruebaandroidjava.R;
+import cl.desafiolatm.pruebaandroidjava.adapter.HeroeAdapter;
 import cl.desafiolatm.pruebaandroidjava.databinding.FragmentListaBinding;
+import cl.desafiolatm.pruebaandroidjava.viewmodel.HeroeViewModel;
 
 public class ListaFragment extends Fragment {
 
     private FragmentListaBinding binding;
+    private HeroeViewModel modelo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentListaBinding.inflate(inflater,container,false);
+        modelo = new ViewModelProvider(getActivity()).get(HeroeViewModel.class);
+        HeroeAdapter adapter = new HeroeAdapter();
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.titulo_fragmento_lista);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 1);
+        binding.rvLista.setAdapter(adapter);
+        binding.rvLista.setLayoutManager(gridLayoutManager);
 
+        adapter.setListener(item -> {
+            modelo.cargarHeroe(item);
+            Navigation.findNavController(getView()).navigate(R.id.action_listaFragment_to_detalleFragment);
+        });
+
+        modelo.getHeroe().observe(getViewLifecycleOwner(),heroe -> {
+            adapter.setLista(heroe.getHeroe());
+        });
+
+        modelo.getToast_error().observe(getViewLifecycleOwner(), error -> {
+            Toast.makeText(getContext(), error, Toast.LENGTH_LONG).show();
+        });
 
         return binding.getRoot();
     }
